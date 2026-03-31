880ee4fd31d82496ae03fabb3e9b387e57238466
AngiE300512 <198231565+AngiE300512@users.noreply.github.com>
2026-03-31 16:44:18 +0530

    Create Reverse the order of words in a sentence1.java

---
+++ Day 7/Reverse the order of words in a sentence1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class SentenceReverse2{
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string value: ");
+        String str1 = sc.nextLine();
+        String out = wordRev2(str1);
+        System.out.print("Output: " + out+"\n");
+
+    }
+
+    static String wordRev2(String s){
+        String out = "", temp = "";
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)!=' '){
+                temp=temp+s.charAt(i);
+            }
+            else{
+                out=temp+" "+out;
+                temp="";
+            }
+            
+            if(i==s.length()-1) out=temp+" "+out;
+        }
+        return out;
+    }
+}
