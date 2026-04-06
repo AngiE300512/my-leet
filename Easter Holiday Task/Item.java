aeb92c13458f0f0854071caa034634e54efc1908
AngiE300512 <198231565+AngiE300512@users.noreply.github.com>
2026-04-06 19:13:37 +0530

    Create Item.java

---
+++ Easter Holiday Task/Item.java
@@ -0,0 +1,13 @@
+abstract class Item{
+String id,name,cat;
+int qty,min;
+double price;
+Item(String i,String n,String c,int q,double p,int m){
+id=i;
+name=n;
+cat=c;
+qty=q;
+price=p;
+min=m;
+}
+}
