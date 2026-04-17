8915a114d7beea5364f1876c19fb8281827eff5b
AngiE300512 <198231565+AngiE300512@users.noreply.github.com>
2026-04-17 18:43:57 +0530

    Create BufferedWriter.java

---
+++ Day 20/BufferedWriter.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+            BufferedWriter bw = new BufferedWriter(new FileWriter("test.txt"));
+            bw.write("Hi");
+            bw.newLine();
+            bw.write("Hello");
+            bw.close();
+       
+    }
+}
