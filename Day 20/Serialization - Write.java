5d9daa7e79fc8381fb64b1cae3606bb3114afbc0
AngiE300512 <198231565+AngiE300512@users.noreply.github.com>
2026-04-17 18:52:52 +0530

    Update Serialization - Write.java

--- Day 20/Serialization - Write.java
+++ Day 20/Serialization - Write.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            Student s1 = new Student("Julie", 25);
+            Student s1 = new Student("Julie",1, 25);
             ObjectOutputStream obj = new ObjectOutputStream(new FileOutputStream("test4.txt"));
             obj.writeObject(s1);
             obj.close();
