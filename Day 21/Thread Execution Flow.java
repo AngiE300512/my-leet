676cc7e32d2d1a1664b19f0ba6014b147977ce5c
AngiE300512 <198231565+AngiE300512@users.noreply.github.com>
2026-04-18 18:56:04 +0530

    Create Thread Execution Flow.java

---
+++ Day 21/Thread Execution Flow.java
@@ -0,0 +1,18 @@
+class Work extends Thread {
+    public void run() {
+        System.out.println("thread 1 is working on run 1");
+    }
+
+    public void run(int a) {
+        System.out.println("thread 1 is working on run 2");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main thread work!!");
+        Work t1 = new Work();
+        t1.start();
+        t1.run(5);
+    }
+}
